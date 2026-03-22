825ec2c3fe358871fee844aa356a5caf62670b67
Adam Worede <adamg2609@gmail.com>
2026-03-22 22:56:27 +0000

    get user

--- querying/create_user.py
+++ querying/create_user.py
@@ -30,14 +30,17 @@ def create_user(
                    """)
 
 
+@database_function
+def get_user(
+    cursor,
+    username
+    ):
+    cursor.execute("""SELECT * FROM user WHERE username = {username}""")
+    row = cursor.fetchone()
+    return row
 
-    
 
-#     CREATE TABLE IF NOT EXISTS users (
-#     username TEXT NOT NULL PRIMARY KEY,
-#     master_password_hash  TEXT NOT NULL
-#     salt TEXT NOT NULL
-#     created_at DATE TIME NOT NULL
-#     updated_at DATE TIME NOT NULL
-# )
+
+
+
     
