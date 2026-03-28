df1917b46fe322d31abcd59765d92194eee00a8b
Adam Worede <adamg2609@gmail.com>
2026-03-28 01:16:41 +0000

    additional database decorators

---
+++ database_decorators/db_fetchall.py
@@ -0,0 +1,25 @@
+import sqlite3
+import os
+
+FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(FILE_PATH, "..", "password_manager.db")
+
+def fetch_all(function):
+    def wrapper(*args, **kwargs):
+        try:
+            connection = sqlite3.connect(DB_PATH)
+            cursor = connection.cursor()
+
+            result = function(*args, **kwargs)
+            query = result[0]
+            params = result[1]
+
+            cursor.execute(query, params)
+
+            db_result = cursor.fetchall()
+
+            return db_result
+        except Exception as e:
+            return e
+        finally:
+            connection.close()
