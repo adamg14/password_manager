b7c9dcda8ac3bd8583e02c73891b7a994b955cd2
Adam Worede <adamg2609@gmail.com>
2026-04-09 22:17:59 +0100

    database transaction wrapper function

--- database_decorators/db_decorator.py
+++ database_decorators/db_decorator.py
@@ -5,6 +5,24 @@ FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(FILE_PATH, "..", "password_manager.db")
 
 
+def transaction_decorator(function):
+    def wrapper(*args, **kwargs):
+        try:
+            connection = sqlite3.connect(DB_PATH)
+            cursor = connection.cursor()
+
+            # function returns a list of queries it needs to be atomic
+            cursor.execute("BEGIN TRANSACTION;")
+            # function runs the queries within the transactions
+            result = function(cursor, *args, **kwargs)
+            cursor.execute("COMMIT;")
+            connection.commit()
+            return result
+        except Exception as e:
+            print(f"Database error: {e}")
+            return None
+        
+        
 def database_wrapper(function):
     def wrapper(*args, **kwargs):
         try:
