ff3b945c463d8c980716b7e5ab4aa08d24590e72
Adam Worede <adamg2609@gmail.com>
2026-05-03 15:07:02 +0100

    local database setup debug

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cli.home import home
+from cli.setup import set_up
 
 if __name__ == '__main__':
+    set_up()
     while True:
         home()
--- test.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("./password_manager.db")
-cursor = connection.cursor()
-query = "SELECT * FROM tablename;"
-cursor.execute(query)
-result = cursor.fetchall()
-print(result)
